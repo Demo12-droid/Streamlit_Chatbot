40faa83958d9705ac1fd83f5dc4f08595c010798
Demo12-droid <ibi632024@gmail.com>
2024-06-26 17:56:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,7 +65,7 @@ with st.form(key='chat_form'):
         user_input = st.text_input("You:",key='user_input')
 
     with button_col:
-        submit_button = st.form_submit_button("",label='Send')
+        submit_button = st.form_submit_button(label='Send')
 
     button_b_pos = "1rem"
     button_css = float_css_helper(bottom=button_b_pos, transition=0.2)
