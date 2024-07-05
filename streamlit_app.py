316a9bd6cb34b2367ae2397652f577526c80453f
godlygodzilla <73699202+godlygodzilla@users.noreply.github.com>
2024-07-05 16:54:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,9 +65,9 @@ st.sidebar.header("Display Options")
 show_plot = st.sidebar.checkbox("Plot",value=False)
 
 if toggle_option == "congestion":
-    st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\n1. What is the average extent in jan 2024\n2. What is the range of the data\n3. What are the details of the highest extent\n4. What has higher average extent jan 2024 or feb 2024\n", unsafe_allow_html=True)
+    st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\nWhat is the average extent in jan 2024\nWhat is the range of the data\nWhat are the details of the highest extent\nWhat has higher average extent jan 2024 or feb 2024\n", unsafe_allow_html=True)
 if toggle_option == "toll_plaza_data":
-    st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\n1. What is the total number of vehicles of type MAV in jan 2024\n2. What is the starting date and the ending date in the data\n3. What is the revenue of different vehicle classes\n4. What is the revenue of different vehicle classes in feb 2024\n5. What is the total number of vehicles in each vehicle class in jan 2024", unsafe_allow_html=True)
+    st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\nWhat is the total number of vehicles of type MAV in jan 2024\nWhat is the starting date and the ending date in the data\nWhat is the revenue of different vehicle classes\nWhat is the revenue of different vehicle classes in feb 2024\nWhat is the total number of vehicles in each vehicle class in jan 2024", unsafe_allow_html=True)
 
 
 if user_input:
