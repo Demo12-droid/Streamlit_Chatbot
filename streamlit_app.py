fddd5c1803c9b3590f72c55a91725edecb47ba16
godlygodzilla <73699202+godlygodzilla@users.noreply.github.com>
2024-07-05 17:03:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -65,11 +65,9 @@ st.sidebar.header("Display Options")
 show_plot = st.sidebar.checkbox("Plot",value=False)
 
 if toggle_option == "congestion":
-    st.button("question 1", type="primary")
-    if st.button("Say hello"):
-        st.write("Why hello there")
-    else:
-        st.write("Goodbye")
+    st.button("What is the average extent in jan 2024", type="primary")
+    #if st.button("Say hello"):
+    #    st.write("Why hello there")
     #st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\n What is the average extent in jan 2024 \n What is the range of the data \n What are the details of the highest extent \n What has higher average extent jan 2024 or feb 2024 \n", unsafe_allow_html=True)
 if toggle_option == "toll_plaza_data":
     st.markdown("<b style='color:orange;'>Try asking the following questions: </b>\n What is the total number of vehicles of type MAV in jan 2024\nWhat is the starting date and the ending date in the data\nWhat is the revenue of different vehicle classes\nWhat is the revenue of different vehicle classes in feb 2024\nWhat is the total number of vehicles in each vehicle class in jan 2024", unsafe_allow_html=True)
