1778ea628de68b6ee584a347233902bbbad9b662
Demo12-droid <ibi632024@gmail.com>
2024-07-31 15:09:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -251,7 +251,7 @@ if st.session_state.logged_in:
 		role = entry.get('role', 'unknown role')
 		content = entry.get('content', {})
 		if role =='user':
-			with st.chat_messgae("User"):
+			with st.chat_message("User"):
 				st.write(content)
 		elif role == 'assistant':
 			sql_query = content.get('sql', None)
