fb7239dbe4e173187e36e945b0f520c1ad9d5bf3
Demo12-droid <ibi632024@gmail.com>
2024-08-05 14:01:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -116,10 +116,11 @@ def display_plot(plot,plot_type):
 			img = pio.from_json(json_string)
 			st.image(img)
 		except:
-			# plot.seek(0)
-			# img = Image.open(img_bytes)
-			fig = pio.from_image(bytes_io)
-			st.image(img)
+			image = Image.open(plot)
+			st.image(image)
+			
+			# fig = pio.from_image(bytes_io)
+			# st.image(img)
 
 	elif plot_type=='Folium Map':
 		components.html(plot,height=390,scrolling=True)
