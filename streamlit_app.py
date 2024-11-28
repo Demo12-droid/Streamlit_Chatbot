2d8fab10c64f4e6d449f71722a73f4bbd687fad8
Demo12-droid <ibi632024@gmail.com>
2024-11-28 14:27:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ users_db = {
 }
 
 def get_response(user_input,show_plot,toggle_option,username,session_id):
-	url = 'https://expert-pigeon-flexible.ngrok-free.app/chat/chatbot/'
+	# url = 'https://diverse-chamois-internally.ngrok-free.app/chat/chatbot/'
+	url = 'https://diverse-chamois-internally.ngrok-free.app/chat/chatbot/'
 	headers = {'Content-Type': 'application/json'}
 	payload = {
 		'username': username,
@@ -49,7 +50,8 @@ def get_response(user_input,show_plot,toggle_option,username,session_id):
 		return None, None, None,None, time_taken
 
 def get_history(username,session_id):
-	url = 'https://expert-pigeon-flexible.ngrok-free.app/chat/session_info/'
+	url = 'https://diverse-chamois-internally.ngrok-free.app/chat/session_info/'
+	
 	headers = {'Content-Type': 'application/json'}
 	payload = {
 		'username': username,
@@ -65,7 +67,7 @@ def get_history(username,session_id):
 		return None
 
 def save_session_id(username,session_id):
-	url = 'https://expert-pigeon-flexible.ngrok-free.app/chat/save_session_id/'
+	url = 'https://diverse-chamois-internally.ngrok-free.app/chat/save_session_id/'
 	headers = {'Content-Type': 'application/json'}
 	payload = {
 		'username': username,
@@ -87,7 +89,7 @@ def authenticate(username, password):
 
 # Function to retrieve session IDs
 def get_session_ids(username):
-	url = 'https://expert-pigeon-flexible.ngrok-free.app/chat/get_session_ids/'
+	url = 'https://diverse-chamois-internally.ngrok-free.app/chat/get_session_ids/'
 	headers = {'Content-Type': 'application/json'}
 	payload = {
 		"username": username,
