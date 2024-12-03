1fd920153de8298ee3511908b95432b2e66e9704
godlygodzilla <73699202+godlygodzilla@users.noreply.github.com>
2024-12-03 12:25:29 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,6 +25,8 @@ users_db = {
 	"user8": {"password": "pass8"},
 	"user9": {"password": "pass9"},
 	"airavt_admin": {"password": "Airavt"},
+	"npci_test": {"password": "npci_test"},
+	"npci_admin": {"password": "npci_admin"),
 }
 
 def get_response(user_input,show_plot,toggle_option,username,session_id):
